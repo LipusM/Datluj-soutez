fea486369d043feb2024f4794a80129ea15b46bd
LipusM <mlipus007@gmail.com>
2023-10-28 11:34:50 +0200

    Stránka statistika připravena

--- src/components/StatisticTime/index.jsx
+++ src/components/StatisticTime/index.jsx
@@ -1,13 +1,19 @@
 const c = console.log.bind(document)
 
+import { NavLink } from "react-router-dom"
+
 import "./style.scss"
 
 const StatisticTime = () => {
 
     return (
-        <>
+        <div id="stage">
             statistika!
-        </>
+
+            <nav id="nav-panel">
+                <NavLink className="navigation" to="/hra-na-cas">Zpět</NavLink>
+            </nav>
+        </div>
     )
 
 }
