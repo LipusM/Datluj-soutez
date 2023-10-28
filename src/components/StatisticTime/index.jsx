944b4f6ea2d5ea42e1d9fcd725e3331f68fa7816
LipusM <mlipus007@gmail.com>
2023-10-28 11:09:20 +0200

    Založení komponenty

---
+++ src/components/StatisticTime/index.jsx
@@ -0,0 +1,15 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+
+const StatisticTime = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default StatisticTime
