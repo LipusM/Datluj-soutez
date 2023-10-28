3078595f82bc1614c350d9e2c378702eee6ed22c
LipusM <mlipus007@gmail.com>
2023-10-28 23:00:57 +0200

    Generování tabulky

--- src/components/StatisticTime/index.jsx
+++ src/components/StatisticTime/index.jsx
@@ -12,21 +12,39 @@ const StatisticTime = () => {
     const loadedGames = JSON.parse(localStorage.getItem("theGames"));
 
     /* if (loadedGames) { */
-      setTheGames(loadedGames);
+    setTheGames(loadedGames);
     /* } */
   }, []);
 
   return (
     <div id="stage">
-
-      {theGames.map((game, index) => (
+      {/* {theGames.map((game, index) => (
         <div key={index}>
           <p>Name: {game.name}</p>
           <p>Mistakes: {game.mistakes}</p>
           <p>Written Words: {game.writtenWords}</p>
           <p>Selected Time: {game.selectedTime}</p>
         </div>
-      ))}
+      ))} */}
+      <table>
+      <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+      {theGames.map((game) => {
+        if (game.selectedTime === 1) {
+          
+            return( 
+              <tr key={game.name}>
+                <td>{game.name}</td>
+                <td>{game.writtenWords}</td>
+                <td>{game.mistakes}</td>
+              </tr>)
+            
+        }
+      })}
+      </table>
 
       <nav id="nav-panel">
         <NavLink className="navigation" to="/hra-na-cas">
